9ee0e9d7c5720a2d054c0ff1e64ea9f848ea6577
sijtech <joesiewerdt@gmail.com>
2018-04-29 19:15:25 +0200

    cleaned up the file content

--- 100-restoreDevEnvironment.sh
+++ 100-restoreDevEnvironment.sh
@@ -6,13 +6,3 @@ chown sij:sij /home/sij/Documents/* -R
 cp -r /tmp/devEnv/2stpf/ /var/www/
 chown www-data:www-data /var/www/2stpf -R
 chmod 775 /var/www/2stpf -R
-
-
-
-#create database myFirstGrid;
-#create user 'jqgrid'@'localhost' identified by 'jqgrid';
-#GRANT ALL PRIVILEGES ON myFirstGrid.* TO 'jqgrid'@'localhost';
-#show grants for 'jqgrid'@'localhost';
-
-#php7.2-mysql
-#libapache2-mod-php
